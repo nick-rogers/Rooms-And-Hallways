2e9ed9c54d66842f60b3abce028afb807e5a654a
nick-rogers <31479865+nick-rogers@users.noreply.github.com>
2018-02-06 11:12:05 -0600

    Uploaded Rooms and Hallways v1

---
+++ Rooms and Hallways/era.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+
+	/*var howMany = Math.round(Math.random()*10);
+
+	for(var i = 0; i < 10; i++){
+		console.log('count ' + i);
+		var randomizedTop = Math.round(Math.random()*350);
+		var randomizedLeft = Math.round(Math.random()*350);
+
+		var flip1 = Math.random();
+		if(flip1 > 0.5){
+			randomizedTop = randomizedTop;
+		}
+		else {
+			randomizedTop = -1 * randomizedTop;
+		}
+
+		var flip2 = Math.random();
+		if(flip2 > 0.5){
+			randomizedLeft = randomizedLeft;
+		}
+		else {
+			randomizedLeft = -1 * randomizedLeft;
+		}
+
+		var div = document.createElement("div");
+		div.style.width = "200px";
+		div.style.height = "110px";
+		div.style.background = "white";
+		div.style.marginLeft = randomizedLeft+"px";
+		div.style.marginTop = randomizedTop+"px";
+		document.getElementById("theBody").appendChild(div);
+	}*/
+});
